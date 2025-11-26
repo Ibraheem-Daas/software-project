78dff8c298c2f875ff4904d606020e2789c6a5b5
ibraa-daas <ibradaas765@gmail.com>
2025-11-27 00:29:25 +0200

    Initial commit before Java 21 upgrade

---
+++ src/test/java/com/example/library/AppTest.java
@@ -0,0 +1,11 @@
+package com.example.library;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    void testApp() {
+        assertTrue(true, "This test should pass");
+    }
+}
